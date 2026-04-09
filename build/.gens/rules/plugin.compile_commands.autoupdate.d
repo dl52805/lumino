023e95ee6858c782bd2d86e3919999ca42f9d1c7
dl52805 <luo524@purdue.edu>
2026-04-09 15:40:42 -0400

    clean up files

--- build/.gens/rules/plugin.compile_commands.autoupdate.d
+++
@@ -1,9 +0,0 @@
-{
-    files = {
-        "/Users/danluo/experiments/cpp/lumino/xmake.lua",
-        "/Users/danluo/experiments/cpp/lumino/.xmake/macosx/arm64/xmake.conf"
-    },
-    values = {
-        "src/main.cpp"
-    }
-}
