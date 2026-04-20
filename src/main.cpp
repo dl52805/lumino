0a786c595c8a0764ec892c557165d8be50aa7aee
dl52805 <luo524@purdue.edu>
2026-04-20 01:20:25 -0400

    load castle model and iterate through the triangles of the model

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <fmt/core.h>
+
 #include <stdio.h>
 
 #include <raylib.h>
@@ -13,10 +15,10 @@ int main(int argc, char **argv)
 
   Camera3D camera = {0};
   camera.position = Vector3 {
-    10.0f, 10.0f, 10.0f,
+    80.0f, 80.0f, 80.0f,
   };
   camera.target = Vector3 {
-    0.0f, 0.0f, 0.0f,
+    0.0f, 12.0f, 0.0f,
   };
   camera.up = Vector3 {
     0.0f, 1.0f, 0.0f,
@@ -24,9 +26,38 @@ int main(int argc, char **argv)
   camera.fovy = 45.0f;
   camera.projection = CAMERA_PERSPECTIVE;
 
-  Vector3 cube_position = {
+  Model model = LoadModel("resources/castle.obj");
+  Texture2D texture = LoadTexture("resources/castle_diffuse.png");
+  model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+
+  Vector3 model_position = {
     0.0f, 0.0f, 0.0f,
   };
+
+  BoundingBox bounds = GetMeshBoundingBox(model.meshes[0]);
+
+  int triangle_count = model.meshes[0].vertexCount / 3;
+  Vector3 *model_vertices = (Vector3 *) model.meshes[0].vertices;
+  for(int i = 0; i < triangle_count; i++)
+  {
+    Vector3 a, b, c;
+    if (model.meshes[0].indices != nullptr)
+    {
+      int idx_a = model.meshes[0].indices[i * 3 + 0];
+      int idx_b = model.meshes[0].indices[i * 3 + 1];
+      int idx_c = model.meshes[0].indices[i * 3 + 2];
+
+      a = model_vertices[idx_a];
+      b = model_vertices[idx_b];
+      c = model_vertices[idx_c];
+    }
+    else
+    {
+      a = model_vertices[i * 3 + 0];
+      b = model_vertices[i * 3 + 1];
+      c = model_vertices[i * 3 + 2];
+    }
+  }
 
   SetTargetFPS(60);
 
@@ -36,7 +67,8 @@ int main(int argc, char **argv)
       ClearBackground(RAYWHITE);
 
       BeginMode3D(camera);
-        DrawCube(cube_position, 2.0f, 2.0f, 2.0f, MAROON);
+        DrawModel(model, model_position, 1.0f, WHITE);
+        DrawGrid(20, 10.0f);
       EndMode3D();
     EndDrawing();
   }
--- xmake.lua
+++ xmake.lua
@@ -11,10 +11,15 @@ add_rules("mode.debug", "mode.release")
 target("lumino")
   set_kind("binary")
   add_files("src/*.cpp")
+  set_rundir("$(projectdir)")
 
   add_includedirs("./lib/raylib/src")
   add_linkdirs("./lib/raylib/src")
   add_links("raylib")
+
+  add_includedirs("./lib/fmtlib/include")
+  add_linkdirs("./lib/fmtlib/bin")
+  add_links("fmt")
 
   if is_os("macosx") then
     add_frameworks("Foundation", "OpenGL", "Cocoa", "IOKit", "CoreAudio")
