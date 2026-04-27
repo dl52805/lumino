285ed49d2c0112f946a17716bd877fee05d8d3b2
Collin James <jamesfamily006@gmail.com>
2026-04-27 11:26:39 -0400

    added windows xmake lua file

---
+++ xmake-win.lua
@@ -0,0 +1,62 @@
+add_rules(
+  "plugin.compile_commands.autoupdate",
+  {
+    outputdir = ".",
+    lsp = "clangd"
+  }
+)
+
+add_rules("mode.debug", "mode.release")
+
+set_defaultmode("debug")
+
+add_requires("raylib", "fmt")
+
+target("lumino")
+  set_kind("binary")
+  add_files("./src/*.cpp")
+  --set_rundir("$(projectdir)")
+  add_packages("raylib", "fmt");
+
+--  add_includedirs("./lib/raylib/src")
+--  add_linkdirs("./lib/raylib/src")
+--  add_links("raylib")
+
+--  add_includedirs("./lib/fmtlib/include")
+--  add_linkdirs("./lib/fmtlib/bin")
+--  add_links("fmt")
+
+  if is_os("macosx") then
+    add_frameworks("Foundation", "OpenGL", "Cocoa", "IOKit", "CoreAudio")
+  elseif is_os("windows") then
+    add_cxxflags("/utf-8")
+    add_links({
+      "OpenGL32",
+      "Gdi32",
+      "WinMM",
+      "kernel32",
+      "shell32",
+      "User32"
+    })
+
+  end
+
+  on_run(
+    function (target)
+      import("core.base.process")
+
+      local stdout = "image.ppm"
+      local proc = process.openv(
+        target:targetfile(),
+        {}, { stdout = stdout }
+      )
+      if proc then
+        proc:wait()
+        proc:close()
+      end
+
+      if is_plat("macosx") then
+        os.exec("magick image.ppm image.png")
+      end
+    end
+  )
