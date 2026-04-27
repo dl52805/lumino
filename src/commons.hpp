eeb0d33c1a74f59dbcb6e5b914788e2f52cad25f
dl52805 <luo524@purdue.edu>
2026-04-27 11:23:05 -0400

    fix build issues for other platforms

---
+++ src/commons.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <limits>
+#include <memory>
+
+using std::make_shared;
+using std::shared_ptr;
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+inline double degrees_to_radians(double degrees)
+{
+  return degrees * pi / 180.0;
+}
+
